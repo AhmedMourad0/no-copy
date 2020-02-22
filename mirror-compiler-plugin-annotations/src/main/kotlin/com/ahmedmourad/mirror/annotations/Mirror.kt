ad9bfd60ae410f8f76f3f9b7d71f1c1f5c603970
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-02-22 20:26:58 +0200

    Mirror is now a constructor annotation instead of a class annotation

--- mirror-compiler-plugin-annotations/src/main/kotlin/com/ahmedmourad/mirror/annotations/Mirror.kt
+++ mirror-compiler-plugin-annotations/src/main/kotlin/com/ahmedmourad/mirror/annotations/Mirror.kt
@@ -1,11 +1,11 @@
 package com.ahmedmourad.mirror.annotations
 
 import kotlin.annotation.AnnotationRetention.BINARY
-import kotlin.annotation.AnnotationTarget.CLASS
+import kotlin.annotation.AnnotationTarget.CONSTRUCTOR
 
 /**
  * An annotation to indicate that the visibility of the copy method of a particular data class should match its constructor's
  */
 @Retention(BINARY)
-@Target(CLASS)
+@Target(CONSTRUCTOR)
 annotation class Mirror
