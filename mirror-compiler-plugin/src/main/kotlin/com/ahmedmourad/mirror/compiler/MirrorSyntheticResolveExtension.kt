9829838832b3d94821c77abe17e9f83ae508eac7
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-02-25 14:38:09 +0200

    Added code for Shatter

--- mirror-compiler-plugin/src/main/kotlin/com/ahmedmourad/mirror/compiler/MirrorSyntheticResolveExtension.kt
+++ mirror-compiler-plugin/src/main/kotlin/com/ahmedmourad/mirror/compiler/MirrorSyntheticResolveExtension.kt
@@ -1,60 +1,94 @@
 package com.ahmedmourad.mirror.compiler
 
-import org.jetbrains.kotlin.descriptors.CallableMemberDescriptor
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.codegen.coroutines.createCustomCopy
+import org.jetbrains.kotlin.descriptors.ClassConstructorDescriptor
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
-import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.descriptors.SimpleFunctionDescriptor
+import org.jetbrains.kotlin.descriptors.Visibilities
 import org.jetbrains.kotlin.descriptors.annotations.Annotated
-import org.jetbrains.kotlin.descriptors.annotations.Annotations
-import org.jetbrains.kotlin.descriptors.impl.SimpleFunctionDescriptorImpl
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.descriptorUtil.classValueType
+import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
 import org.jetbrains.kotlin.resolve.extensions.SyntheticResolveExtension
 
-/**
- * A [SyntheticResolveExtension] that replaces the open toString descriptor
- * with a final descriptor for data classes.
- */
 class MirrorSyntheticResolveExtension(
-    private val fqMirrorAnnotation: FqName
+        private val messageCollector: MessageCollector,
+        private val fqMirrorAnnotation: FqName,
+        private val fqShatterAnnotation: FqName
 ) : SyntheticResolveExtension {
 
     override fun generateSyntheticMethods(
-        thisDescriptor: ClassDescriptor,
-        name: Name,
-        bindingContext: BindingContext,
-        fromSupertypes: List<SimpleFunctionDescriptor>,
-        result: MutableCollection<SimpleFunctionDescriptor>
+            thisDescriptor: ClassDescriptor,
+            name: Name,
+            bindingContext: BindingContext,
+            fromSupertypes: List<SimpleFunctionDescriptor>,
+            result: MutableCollection<SimpleFunctionDescriptor>
     ) {
         super.generateSyntheticMethods(thisDescriptor, name, bindingContext, fromSupertypes, result)
 
-        val constructor = thisDescriptor.constructors.firstOrNull { it.isPrimary } ?: return
-        if (name.asString() == "copy" && thisDescriptor.isMirrored(fqMirrorAnnotation)) {
-            // Remove the open toString descriptor
-            result.clear()
-            // Add a final toString descriptor
-            result += SimpleFunctionDescriptorImpl.create(
-                thisDescriptor,
-                Annotations.EMPTY,
-                name,
-                CallableMemberDescriptor.Kind.SYNTHESIZED,
-                thisDescriptor.source
-            ).initialize(
-                null,
-                thisDescriptor.thisAsReceiverParameter,
-                emptyList(),
-                emptyList(),
-                thisDescriptor.classValueType,
-                Modality.FINAL,
-                constructor.visibility
-            )
+        val hasShatterAnnotation = thisDescriptor.hasAnnotation(fqShatterAnnotation)
+
+        if (!thisDescriptor.isData) {
+
+            if (thisDescriptor.constructors.any { it.hasAnnotation(fqMirrorAnnotation) }) {
+                error("Only data classes could be annotated with Mirror (${thisDescriptor.fqNameOrNull()})")
+            }
+
+            if (hasShatterAnnotation) {
+                error("Only data classes could be annotated with Shatter (${thisDescriptor.fqNameOrNull()})")
+            }
+
+            return
         }
+
+        if (name.asString() != "copy") {
+            return
+        }
+
+        val mirrorConstructors = thisDescriptor.constructors.filter { it.hasAnnotation(fqMirrorAnnotation) }
+
+        if (mirrorConstructors.size > 1) {
+            error("You cannot have more than one Mirror annotated constructors (${thisDescriptor.fqNameOrNull()})")
+        }
+
+        val hasMirrorAnnotation = mirrorConstructors.isNotEmpty()
+
+        if (hasMirrorAnnotation && hasShatterAnnotation) {
+            error("You cannot have Mirror and Shatter for the same data class (${thisDescriptor.fqNameOrNull()})")
+        }
+
+        if (hasShatterAnnotation) {
+            handleShatter(result)
+        } else if (hasMirrorAnnotation) {
+            handleMirror(thisDescriptor, mirrorConstructors[0], result)
+        }
+    }
+
+    private fun handleShatter(result: MutableCollection<SimpleFunctionDescriptor>) {
+        result.clear()
+    }
+
+    private fun handleMirror(
+            thisDescriptor: ClassDescriptor,
+            constructor: ClassConstructorDescriptor,
+            result: MutableCollection<SimpleFunctionDescriptor>
+    ) {
+
+        if (constructor.visibility == Visibilities.INTERNAL) {
+            error("Mirroring internal constructors is not currently supported, try Shatter instead (${thisDescriptor.fqNameOrNull()})")
+        }
+
+        val newCopy = result.firstOrNull()
+                ?.createCustomCopy { it.newCopyBuilder().setVisibility(constructor.visibility) }
+
+        result.clear()
+        newCopy?.let(result::add) ?: error("Couldn't mirror constructor (${thisDescriptor.fqNameOrNull()})")
     }
 }
 
-internal fun Annotated.isMirrored(redactedAnnotation: FqName): Boolean {
-    return annotations.hasAnnotation(redactedAnnotation)
+internal fun Annotated.hasAnnotation(mirrorAnnotation: FqName): Boolean {
+    return annotations.hasAnnotation(mirrorAnnotation)
 }
 
