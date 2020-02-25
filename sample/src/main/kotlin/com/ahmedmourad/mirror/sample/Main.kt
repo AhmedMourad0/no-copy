4d2de984e30ebceb72ab4d5b28fa6c4c1fbdfea0
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-02-25 10:05:04 +0200

    Added a sample

---
+++ sample/src/main/kotlin/com/ahmedmourad/mirror/sample/Main.kt
@@ -0,0 +1,9 @@
+package com.ahmedmourad.mirror.sample
+
+fun main() {
+    println(User.of(
+            "",
+            "",
+            ""
+    ).copy(name = "scs"))
+}
---
+++ sample/src/main/kotlin/com/ahmedmourad/mirror/sample/User.kt
@@ -0,0 +1,20 @@
+package com.ahmedmourad.mirror.sample
+
+import com.ahmedmourad.mirror.annotations.Mirror
+
+//@Shatter
+data class User @Mirror internal constructor(
+        val name: String,
+        val email: String,
+        val phoneNumber: String
+) {
+    constructor() : this("", "", "")
+
+    internal fun x() = 1
+
+    companion object {
+        fun of(name: String, email: String, phoneNumber: String): User {
+            return User(name, email, phoneNumber)
+        }
+    }
+}
