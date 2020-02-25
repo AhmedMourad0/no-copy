bedd985064b2ca675d49048d158413bd704142f6
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-02-25 12:25:10 +0200

    Mirror now supports both constructors and classes

--- mirror-compiler-plugin-annotations/src/main/kotlin/com/ahmedmourad/mirror/annotations/Mirror.kt
+++ mirror-compiler-plugin-annotations/src/main/kotlin/com/ahmedmourad/mirror/annotations/Mirror.kt
@@ -1,11 +1,13 @@
 package com.ahmedmourad.mirror.annotations
 
 import kotlin.annotation.AnnotationRetention.BINARY
+import kotlin.annotation.AnnotationTarget.CLASS
 import kotlin.annotation.AnnotationTarget.CONSTRUCTOR
 
 /**
- * An annotation to indicate that the visibility of the copy method of a particular data class should match its constructor's
+ * An annotation to indicate that the visibility of the copy method of a particular
+ * data class should match that of this constructor
  */
 @Retention(BINARY)
-@Target(CONSTRUCTOR)
+@Target(CLASS, CONSTRUCTOR)
 annotation class Mirror
