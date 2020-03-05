710d18b7736383158ed9f7d51f3473a1cec83988
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-03-05 16:19:29 +0200

    Moved User to Main.kt

--- sample/src/main/kotlin/com/ahmedmourad/mirror/sample/Main.kt
+++ sample/src/main/kotlin/com/ahmedmourad/mirror/sample/Main.kt
@@ -1,9 +1,27 @@
 package com.ahmedmourad.mirror.sample
 
+import com.ahmedmourad.mirror.annotations.Shatter
+
+@Shatter
+data class User /*@Mirror*/ private constructor(
+        val name: String,
+        val email: String,
+        val phoneNumber: String
+) {
+    constructor() : this("", "", "")
+
+    fun fcsamir() = name + email
+
+    //    fun copy(name: String="", email: String="", phoneNumber: String="") {
+//
+//    }
+    companion object {
+        fun of(name: String, email: String, phoneNumber: String): User {
+            return User(name, email, phoneNumber)
+        }
+    }
+}
+
 fun main() {
-    println(User.of(
-            "",
-            "",
-            ""
-    ).copy(name = "scs"))
+    println(User.of("", "", "").copy(name = "scs"))
 }
