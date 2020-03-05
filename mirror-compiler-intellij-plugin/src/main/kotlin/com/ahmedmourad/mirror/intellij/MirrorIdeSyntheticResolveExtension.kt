b4338cef6e02f8326f68b6381b3cd9c86e0171ed
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-03-05 16:17:23 +0200

    Created MirrorIdeSyntheticResolveExtension.

---
+++ mirror-compiler-intellij-plugin/src/main/kotlin/com/ahmedmourad/mirror/intellij/MirrorIdeSyntheticResolveExtension.kt
@@ -0,0 +1,12 @@
+package com.ahmedmourad.mirror.intellij
+
+import com.ahmedmourad.mirror.core.Strategy
+import com.intellij.openapi.project.Project
+import org.jetbrains.kotlin.name.FqName
+import com.ahmedmourad.mirror.compiler.MirrorSyntheticResolveExtension as MirrorCompilerSyntheticResolveExtension
+
+open class MirrorIdeSyntheticResolveExtension(project: Project) : MirrorCompilerSyntheticResolveExtension(
+        FqName("com.ahmedmourad.mirror.annotations.Mirror"),
+        FqName("com.ahmedmourad.mirror.annotations.Shatter"),
+        Strategy.BY_ANNOTATIONS
+)
