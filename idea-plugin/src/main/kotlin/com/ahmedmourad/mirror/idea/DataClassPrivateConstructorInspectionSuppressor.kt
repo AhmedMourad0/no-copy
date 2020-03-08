68a6fc9c1fb6b0a03f509da490953c5fd6e95244
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-03-08 21:49:42 +0200

    Renamed DataPrivateConstructorInspectionSuppressor.kt to DataClassPrivateConstructorInspectionSuppressor.kt

---
+++ idea-plugin/src/main/kotlin/com/ahmedmourad/mirror/idea/DataClassPrivateConstructorInspectionSuppressor.kt
@@ -0,0 +1,20 @@
+package com.ahmedmourad.mirror.idea
+
+import com.intellij.codeInspection.InspectionSuppressor
+import com.intellij.codeInspection.SuppressQuickFix
+import com.intellij.psi.PsiElement
+
+class DataClassPrivateConstructorInspectionSuppressor : InspectionSuppressor {
+
+    override fun isSuppressedFor(element: PsiElement, toolId: String): Boolean {
+        return toolId == TOOL_ID
+    }
+
+    override fun getSuppressActions(element: PsiElement?, toolId: String): Array<SuppressQuickFix> {
+        return SuppressQuickFix.EMPTY_ARRAY
+    }
+
+    companion object {
+        private const val TOOL_ID = "DataClassPrivateConstructor"
+    }
+}
