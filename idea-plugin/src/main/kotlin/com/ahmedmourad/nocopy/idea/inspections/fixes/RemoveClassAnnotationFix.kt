af5fdae4dc9774c83df2f053ed53a6d20f6cfb14
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-03-16 00:22:21 +0200

    Created RemoveClassAnnotationFix.kt

---
+++ idea-plugin/src/main/kotlin/com/ahmedmourad/nocopy/idea/inspections/fixes/RemoveClassAnnotationFix.kt
@@ -0,0 +1,29 @@
+package com.ahmedmourad.nocopy.idea.inspections.fixes
+
+import com.intellij.codeInsight.FileModificationService
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.openapi.project.Project
+import org.jetbrains.kotlin.idea.core.deleteElementAndCleanParent
+import org.jetbrains.kotlin.idea.util.findAnnotation
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
+
+class RemoveClassAnnotationFix(private val annotation: FqName) : LocalQuickFix {
+
+    override fun getName() = "Remove @${annotation.shortName()} from class"
+
+    override fun getFamilyName() = "Remove annotation from class "
+
+    override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+
+        val containingClass = descriptor.startElement.getParentOfType<KtClass>(strict = false)
+
+        if (!FileModificationService.getInstance().preparePsiElementForWrite(containingClass)) {
+            return
+        }
+
+        containingClass?.findAnnotation(annotation)?.deleteElementAndCleanParent()
+    }
+}
