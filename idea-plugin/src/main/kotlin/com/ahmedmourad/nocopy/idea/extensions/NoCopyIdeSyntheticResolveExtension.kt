8a58fe486adc51c42f7eef5f812686c45c285618
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-03-17 13:16:21 +0200

    Removed the unused Project parameter

--- idea-plugin/src/main/kotlin/com/ahmedmourad/nocopy/idea/extensions/NoCopyIdeSyntheticResolveExtension.kt
+++ idea-plugin/src/main/kotlin/com/ahmedmourad/nocopy/idea/extensions/NoCopyIdeSyntheticResolveExtension.kt
@@ -1,11 +1,8 @@
 package com.ahmedmourad.nocopy.idea.extensions
 
-import com.intellij.openapi.project.Project
 import com.ahmedmourad.nocopy.compiler.NoCopySyntheticResolveExtension as NoCopyCompilerSyntheticResolveExtension
 
-open class NoCopyIdeSyntheticResolveExtension(
-        project: Project
-) : NoCopyCompilerSyntheticResolveExtension() {
+open class NoCopyIdeSyntheticResolveExtension : NoCopyCompilerSyntheticResolveExtension() {
     override fun onError(message: String) {
         // Don't throw an exception
     }
