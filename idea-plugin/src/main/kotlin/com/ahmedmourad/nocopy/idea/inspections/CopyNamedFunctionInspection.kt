2e798daf4a21592a37e9debe19405704d11b37e0
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-03-17 13:23:40 +0200

    Created CopyNamedFunctionInspection.kt

---
+++ idea-plugin/src/main/kotlin/com/ahmedmourad/nocopy/idea/inspections/CopyNamedFunctionInspection.kt
@@ -0,0 +1,45 @@
+package com.ahmedmourad.nocopy.idea.inspections
+
+import com.ahmedmourad.nocopy.idea.utils.hasLeastVisibleCopy
+import com.ahmedmourad.nocopy.idea.utils.hasNoCopy
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
+import org.jetbrains.kotlin.psi.namedFunctionVisitor
+import org.jetbrains.kotlin.psi.psiUtil.containingClass
+
+class CopyNamedFunctionInspection : AbstractKotlinInspection() {
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+
+        return namedFunctionVisitor { function ->
+
+            val containingClass = function.containingClass() ?: return@namedFunctionVisitor
+            val hasNoCopy = containingClass.hasNoCopy()
+            val hasLeastVisibleCopy = containingClass.hasLeastVisibleCopy()
+
+            if (function.name == "copy" && containingClass.isData() && hasNoCopy != hasLeastVisibleCopy) {
+
+                var annotationName = "no-copy"
+
+                if (hasNoCopy) {
+                    annotationName = "@NoCopy"
+                } else if (hasLeastVisibleCopy) {
+                    annotationName = "@LeastVisibleCopy"
+                }
+
+                val identifier = function.nameIdentifier ?: return@namedFunctionVisitor
+                val problemDescriptor = holder.manager.createProblemDescriptor(
+                        identifier,
+                        identifier,
+                        "NoCopy: Having `copy` named methods in $annotationName annotated classes is not yet supported.",
+                        ProblemHighlightType.GENERIC_ERROR,
+                        isOnTheFly
+                )
+
+                holder.registerProblem(problemDescriptor)
+            }
+        }
+    }
+}
