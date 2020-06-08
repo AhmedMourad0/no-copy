98c8e4e8c53de5f6da1d07a492e4c11903c0132d
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-06-08 12:27:59 +0200

    Changed annotation retention from `BINARY` to `SOURCE`

--- plugins/annotations/src/main/kotlin/dev/ahmedmourad/nocopy/annotations/LeastVisibleCopy.kt
+++ plugins/annotations/src/main/kotlin/dev/ahmedmourad/nocopy/annotations/LeastVisibleCopy.kt
@@ -1,12 +1,12 @@
 package dev.ahmedmourad.nocopy.annotations
 
-import kotlin.annotation.AnnotationRetention.BINARY
+import kotlin.annotation.AnnotationRetention.SOURCE
 import kotlin.annotation.AnnotationTarget.CLASS
 
 /**
  * An annotation to indicate that the visibility of the copy method of a particular
  * data class should match that of the least visible constructor
  */
-@Retention(BINARY)
+@Retention(SOURCE)
 @Target(CLASS)
 annotation class LeastVisibleCopy
--- plugins/annotations/src/main/kotlin/dev/ahmedmourad/nocopy/annotations/NoCopy.kt
+++ plugins/annotations/src/main/kotlin/dev/ahmedmourad/nocopy/annotations/NoCopy.kt
@@ -1,11 +1,11 @@
 package dev.ahmedmourad.nocopy.annotations
 
-import kotlin.annotation.AnnotationRetention.BINARY
+import kotlin.annotation.AnnotationRetention.SOURCE
 import kotlin.annotation.AnnotationTarget.CLASS
 
 /**
  * An annotation to indicate that the copy method of a particular data class won't be generated
  */
-@Retention(BINARY)
+@Retention(SOURCE)
 @Target(CLASS)
 annotation class NoCopy
