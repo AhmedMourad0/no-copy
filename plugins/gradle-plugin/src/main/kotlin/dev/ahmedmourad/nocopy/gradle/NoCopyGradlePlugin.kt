174062790fcb0943d6777191cc7f50bb1197b00d
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-06-17 00:24:08 +0200

    Removed unused functions

--- plugins/gradle-plugin/src/main/kotlin/dev/ahmedmourad/nocopy/gradle/NoCopyGradlePlugin.kt
+++ plugins/gradle-plugin/src/main/kotlin/dev/ahmedmourad/nocopy/gradle/NoCopyGradlePlugin.kt
@@ -1,20 +1,13 @@
 package dev.ahmedmourad.nocopy.gradle
 
 import dev.ahmedmourad.nocopy.core.PLUGIN_NAME
-import io.github.classgraph.ClassGraph
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import java.io.File
 
 class NoCopyGradlePlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.extensions.create(PLUGIN_NAME, NoCopyGradleExtension::class.java)
     }
-}
-
-private fun classpathOf(dependency: String): File {
-    val regex = Regex(".*${dependency.replace(':', '-')}.*")
-    return ClassGraph().classpathFiles.first { classpath -> classpath.name.matches(regex) }
 }
 
 open class NoCopyGradleExtension
