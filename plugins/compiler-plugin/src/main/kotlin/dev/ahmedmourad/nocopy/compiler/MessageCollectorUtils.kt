4f750d456f79a9178e8c91fcf70a1e770af534ff
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2021-05-10 00:35:26 +0200

    Added `NoCopy: ` prefix to log and error messages

--- plugins/compiler-plugin/src/main/kotlin/dev/ahmedmourad/nocopy/compiler/MessageCollectorUtils.kt
+++ plugins/compiler-plugin/src/main/kotlin/dev/ahmedmourad/nocopy/compiler/MessageCollectorUtils.kt
@@ -6,18 +6,20 @@ import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.cli.common.messages.MessageUtil
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 
+internal const val LOG_TAG = "NoCopy: "
+
 internal fun MessageCollector.error(message: String, element: PsiElement?) {
     this.report(
-            CompilerMessageSeverity.ERROR,
-            message,
-            MessageUtil.psiElementToMessageLocation(element)
+        CompilerMessageSeverity.ERROR,
+        "$LOG_TAG$message",
+        MessageUtil.psiElementToMessageLocation(element)
     )
 }
 
 internal fun MessageCollector.log(message: String) {
     this.report(
-            CompilerMessageSeverity.LOGGING,
-            "NO_COPY: $message",
-            CompilerMessageLocation.create(null)
+        CompilerMessageSeverity.LOGGING,
+        "$LOG_TAG$message",
+        CompilerMessageLocation.create(null)
     )
 }
